cd8fc543f543059d8376d5c2081230287379a6a4
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-05-27 13:17:27 +0600

    courses&users-schema&modelcreate-router-setup-vdo23

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ config({
 }) //confign.env te port 4000, connection er jonno
 const app=express()
 
+//router setup
+import course from './routes/CourseRoute.js' //note: .js dewa lagbey na hole error dibe
+import users from './routes/UserRoute.js'
+app.use("/api/v1",course)
+app.use("/api/v1", users)
+
 export default app
---
+++ routes/UserRoute.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { getAllUsers } from '../controllers/userController.js'
+
+const router= express.Router()
+router.route("/users").get(getAllUsers)
+
+export default router
