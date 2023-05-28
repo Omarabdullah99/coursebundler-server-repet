b6ac41491e58b0ae29521ab5216ed4aece05c0fd
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-05-28 09:58:41 +0600

    loginandlogout-vdo28

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,3 +27,38 @@ import { sendToken } from "../utils/sendToken.js";
     sendToken(res,user, "Register Successfully", 201)
 
  })
+
+
+//login controller
+ export const login =catchAsyncError(async(req,res,next)=>{
+    const {email,password}=req.body;
+    
+    if( !email || !password )
+    return next(new ErrorHandler("please enter all register field",400));
+
+    const user=await User.findOne({email}).select("+password")
+    if(!user) return next(new ErrorHandler("User dosen't Exist", 401))
+
+    const isMatch=await user.comparePassword(password)
+    if(!isMatch) return next(new ErrorHandler("Incorrect Email or Password",401))
+
+  
+
+    sendToken(res,user, `Welcome back, ${user.name}`, 200)
+
+ })
+
+ //logout controller
+
+ export const logout=catchAsyncError(async (req,res,next)=>{
+    res.status(200).cookie("token",null,{
+        expires:new Date(Date.now()),
+        httpOnly:true,
+        // secure:true, //fontend means 3000 hole true,postman check korar somoy secure:true comment hobe
+        sameSite:"none",
+    }).json({
+        success:true,
+        message:"Logout successfully"
+
+    })
+})
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import jwt from 'jsonwebtoken'
 import validator from 'validator'
+import bcrypt from 'bcrypt'
 
 //user schema create
 const schema= new mongoose.Schema({
@@ -62,11 +63,26 @@ const schema= new mongoose.Schema({
 
 })
 
+//regitation korar somoy password jate hase dekay. it's mean main password na dekay
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+  
+});
+
 //jwt token create, ei function er connection utils-> sendToken.js e 
 schema.methods.getJWTToken= function(){
   return jwt.sign({_id: this._id},process.env.JWT_SECRET,{
     expiresIn:"15d"
   })
+}
+
+//compare passowrd function ta userController e login e use hoise
+schema.methods.comparePassword=async function(password){
+  // console.log(this.password)
+  return await bcrypt.compare(password, this.password)
+
 }
 
 //user model create
